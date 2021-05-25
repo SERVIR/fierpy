993a5c1837c58870eee2d447b079e12558df22fb
kmarkert <kel.markert@gmail.com>
2021-05-25 16:55:46 -0500

    path version bump -> 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='fierpy',
-    version='0.0.3',
+    version='0.0.4',
     description='Python implementation of the Forecasting Inundation Extents using REOF method',
     long_description=long_description,
     long_description_content_type="text/markdown",
